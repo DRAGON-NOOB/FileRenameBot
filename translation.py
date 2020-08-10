6728147a45d203e8ac8b1f67e50e3b00b55da70e
DRAGON-NOOB <69449962+DRAGON-NOOB@users.noreply.github.com>
2020-08-10 11:48:25 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,28 +2,28 @@ class Translation(object):
     START_TEXT = """ <b>I'm A Simple File Renamer Bot With Permanent Thumbnail support!💯</b>
 
 <b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>
-<b>Bot Maintained By: @Mai_bOTs</b>"""
+<b>Bot Maintained By: @noob_admin</b>"""
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
+    UPGRADE_TEXT = "CONTACT @Noob_admin "
     DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me JOin > @Mai_bOTs  ❤️**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
+    NOT_AUTH_USER_TEXT = "CONTACT @Noob_admin "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @No_OnE_Kn0wS_Me "
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /deletethumbnail❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    SAVED_RECVD_DOC_FILE = "<b>⚡File Downloaded Successfully ⚡</b>"
     REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Me >> /n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Thumbnails All The Time!!</b> \n\n `Plan`: <b>Free User</b> \n `Plan Expires On` : <b>You Can Use Meh Until I'm Ded ^_^</b>"""
+    ABOUT_ME = """<b>Me >> /n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Thumbnails All The Time!!</b> \n\n `Plan`: <b>Free User</b> \n `Plan Expires On` : <b>You Can Use Meh Until I'm Ded : <b>source code: [click here](https://t.me/AdhavaaBiriyaniKittiyalo) ^_^</b>"""
     HELP_USER = """Ohh You Want Help?? 😅
     
 1.🔹 <b>Send me any Telegram File.</b>
